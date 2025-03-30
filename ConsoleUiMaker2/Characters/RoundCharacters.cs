fcfde473c5aa714bb9ecb0afbe14c6005eab3418
mathe-man <mathieu.workcontact@gmail.com>
2025-03-30 10:54:31 +0300

    Created Debug ui and characters, changed characters value in enums to hexadecimal num

--- ConsoleUiMaker2/Characters/RoundCharacters.cs
+++ ConsoleUiMaker2/Characters/RoundCharacters.cs
@@ -2,21 +2,21 @@ namespace ConsoleUiMaker2.Characters;
 
 public enum RoundCharacters
 {
-    TopLeftCorner = '\u250c',
-    TopRightCorner = '\u2510',
-    BottomLeftCorner = '\u2514',
-    BottomRightCorner = '\u2518',
+    TopLeftCorner = 0x250c,
+    TopRightCorner = 0x2510,
+    BottomLeftCorner = 0x2514,
+    BottomRightCorner = 0x2518,
     
     
-    Cross = '\u253c',
-    Horizontal = '\u2500',
-    ThickHorizontal = '\u2501',
-    Vertical = '\u2502',
-    ThickVertical = '\u2503',
+    Cross = 0x253c,
+    Horizontal = 0x2500,
+    ThickHorizontal = 0x2501,
+    Vertical = 0x2502,
+    ThickVertical = 0x2503,
     
-    Space = ' ',
+    Space = 0x0020,
     
-    EmptyCircle = '\u25ef',
-    FilledCircle = '\u25cf',
-    SmallEmptyCircle = '\u25cb',
+    EmptyCircle = 0x25ef,
+    FilledCircle = 0x25cf,
+    SmallEmptyCircle = 0x25cb,
 }
--- Debug/Program.cs
+++ Debug/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using ConsoleUiMaker2;
+using ConsoleUiMaker2.Characters;
 using ConsoleUiMaker2.UiElement;
 
 namespace Debug;
@@ -8,10 +9,31 @@ public static class Program
 {
     public static void Main(string[] args)
     {
+        DebugCharacters();
+    }
+
+
+    public static void DebugUi()
+    {
         Text myFirstText = new Text(["One line"], 4, 2);
         Text mySecondText = new Text(["Two", "lines"], 1, 4);
 
         myFirstText.Length = 30;
         ConsoleUi.RunUi();
+    }
+
+    public static void DebugCharacters()
+    {
+        Console.WriteLine("Box characters: " + (char)BoxCharacters.TopLeftCorner);
+        foreach (var character in Enum.GetValues(typeof(BoxCharacters)))
+        {
+            Console.WriteLine($"{character}: {(char)character}");
+        }
+        
+        Console.WriteLine("Round characters: ");
+        foreach (var character in Enum.GetValues(typeof(RoundCharacters)))
+        {
+            Console.Write(character);
+        }
     }
 }
