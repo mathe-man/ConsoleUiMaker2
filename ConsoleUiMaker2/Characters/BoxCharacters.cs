fe0410ae2ece009129b69bfbe672479f8e509d0d
mathe-man <mathieu.workcontact@gmail.com>
2025-03-30 10:36:29 +0300

    Add squares with different shade and RoundCharacters enum

---
+++ ConsoleUiMaker2/Characters/BoxCharacters.cs
@@ -0,0 +1,31 @@
+namespace ConsoleUiMaker2.Characters;
+
+public enum BoxCharacters
+{
+    TopLeftCorner = '\u250c',
+    ThickTopLeftCorner = '\u250f',
+    TopRightCorner = '\u2510',
+    ThickTopRightCorner = '\u2513',
+    BottomLeftCorner = '\u2514',
+    ThickBottomLeftCorner = '\u2517',
+    BottomRightCorner = '\u2518',
+    ThickBottomRightCorner = '\u251b',
+    
+    UpT = '\u252c',
+    DownT = '\u2534',
+    LeftT = '\u251c',
+    RightT = '\u2524',
+    
+    Cross = '\u253c',
+    Horizontal = '\u2500',
+    ThickHorizontal = '\u2501',
+    Vertical = '\u2502',
+    ThickVertical = '\u2503',
+    
+    Space = ' ',
+
+    LightShadeSquare = '\u2591',
+    MediumShadeSquare = '\u2592',
+    DarkShadeSquare = '\u2593',
+    FilledSquare = '\u2588',
+}
---
+++ ConsoleUiMaker2/Characters/RoundCharacters.cs
@@ -0,0 +1,22 @@
+namespace ConsoleUiMaker2.Characters;
+
+public enum RoundCharacters
+{
+    TopLeftCorner = '\u250c',
+    TopRightCorner = '\u2510',
+    BottomLeftCorner = '\u2514',
+    BottomRightCorner = '\u2518',
+    
+    
+    Cross = '\u253c',
+    Horizontal = '\u2500',
+    ThickHorizontal = '\u2501',
+    Vertical = '\u2502',
+    ThickVertical = '\u2503',
+    
+    Space = ' ',
+    
+    EmptyCircle = '\u25ef',
+    FilledCircle = '\u25cf',
+    SmallEmptyCircle = '\u25cb',
+}
