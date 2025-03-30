236e0566e38a430230b726d4410651d80c747de5
mathe-man <mathieu.workcontact@gmail.com>
2025-03-30 10:22:42 +0300

    Add characters for BoxCharacters enum

--- ConsoleUiMaker2/Characters/Characters.cs
+++ ConsoleUiMaker2/Characters/Characters.cs
@@ -1,6 +1,18 @@
 namespace ConsoleUiMaker2.Characters;
 
-public enum Characters
+public enum BoxCharacters
 {
+    TopLeft = '\u250c',
+    TopRight = '\u2510',
+    BottomLeft = '\u2514',
+    BottomRight = '\u2518',
     
+    UpT = '\u252c',
+    DownT = '\u2534',
+    LeftT = '\u251c',
+    RightT = '\u2524',
+    
+    Cross = '\u253c',
+    Horizontal = '\u2500',
+    Vertical = '\u2502'
 }
